d6a5d0122b56a6d023252156171c02526886cb41
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-10-10 14:46:12 +0200

    maj

--- installUser.sh
+++ installUser.sh
@@ -11,5 +11,5 @@ mkdir "$sshLocal"
 chmod 0700 "$sshLocal"
 
 defaultGithubUser="admin-siweb"
-curl "https://api.github.com/usrs/$defaultGithubUser/keys" | jq -r .[].key >> $sshLocal/authorized_keys
+curl "https://api.github.com/users/$defaultGithubUser/keys" | jq -r .[].key >> $sshLocal/authorized_keys
 chown -R $defaultUser:$defaultUser $sshLocal
