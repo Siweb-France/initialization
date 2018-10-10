8fe74bb069ce775301d4f74ff398907cf0774bda
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-10-10 14:59:04 +0200

    add it siweb to www group

--- installUser.sh
+++ installUser.sh
@@ -13,3 +13,5 @@ chmod 0700 "$sshLocal"
 defaultGithubUser="admin-siweb"
 curl "https://api.github.com/users/$defaultGithubUser/keys" | jq -r .[].key >> $sshLocal/authorized_keys
 chown -R $defaultUser:$defaultUser $sshLocal
+
+usermod -a -G www-data it-siweb
