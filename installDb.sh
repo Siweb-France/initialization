06dd967f7e737451fae90b82bc95021ec033f442
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-10-11 16:02:48 +0200

    ask for password directly

--- installDb.sh
+++ installDb.sh
@@ -2,20 +2,14 @@
 
 set -euo pipefail
 
-if test "$#" -lt 2;
-then
-    >&2 echo "Error: Usage: $0 username password"
-    exit 1;
-fi
-
-username="$1"
-password="$2"
+read -p "Username ? " username
+read -sp "Password ?" password
 
 echo "mysql-server mysql-server/root_password password $password" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $password" | debconf-set-selections
 
 apt-get install mysql-server -y
-mysql -h localhost -u $username --password=$password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$password' WITH GRANT OPTION;"
-mysql -h localhost -u $username --password=$password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$password' WITH GRANT OPTION;"
+mysql -h localhost -u $username --password=$password -e "GRANT ALL PRIVILEGES ON *.* TO '$username'@'localhost' IDENTIFIED BY '$password' WITH GRANT OPTION;"
+mysql -h localhost -u $username --password=$password -e "GRANT ALL PRIVILEGES ON *.* TO '$username'@'%' IDENTIFIED BY '$password' WITH GRANT OPTION;"
 
 mv /etc/mysql/my.cnf{,.origine}
