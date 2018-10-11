24b32618db2bc9397b7636aabb193402992a691d
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-10-11 10:59:12 +0200

    add mac change

--- installSsh.sh
+++ installSsh.sh
@@ -2,6 +2,7 @@
 
 
 sed -i -e "s/PermitRootLogin yes/PermitRootLogin without-password/g" /etc/ssh/sshd_config
+sed -i -E "s/(MAC .*)/\0,hmac-ripemd160/g" /etc/ssh/sshd_config
 
 service ssh restart
 
